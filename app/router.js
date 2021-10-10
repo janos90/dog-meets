fcfccdaaf8e8b1cbc71291ddddb945a27a3c083b
Juan <garcij07@myunitec.ac.nz>
2021-10-11 08:15:33 +1300

    added authenticated route

--- app/router.js
+++ app/router.js
@@ -8,9 +8,12 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('login');
-  this.route('dogs');
-  this.route('profile');
-  this.route('activities');
-  this.route('activity', { path: 'activity/:activity_id' });
-  this.route('dog', { path: 'dog/:dog_id' });
+  this.route('authenticated', { path: '' }, function(){
+    this.route('dogs');
+    this.route('profile');
+    this.route('activities');
+    this.route('activity', { path: 'activity/:activity_id' });
+    this.route('dog', { path: 'dog/:dog_id' });
+  });
+
 });
